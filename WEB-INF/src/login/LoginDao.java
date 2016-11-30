8ab2d3dc0fc34386f56d0e41f3056a9ce97dac47
palaw <palaw@users.noreply.github.com>
2016-12-01 04:26:09 +0900

    [update]LoginDao.javaの分岐処理を変更

--- WEB-INF/src/login/LoginDao.java
+++ WEB-INF/src/login/LoginDao.java
@@ -15,9 +15,9 @@ public class LoginDao {
 
     private Connection con = null;
     private ResultSet rs = null;
-    private PreparedStatement ps = null;
 
-    private final String SQL_ID = "kunugi@localhost";
+    private final String SQL_URL = "jdbc:mysql://localhost:3306/hamfile?autoReconnect=true&useSSL=false";
+    private final String SQL_ID = "kunugi";
     private final String SQL_PASS = "hamuro";
 
     /**
@@ -27,27 +27,25 @@ public class LoginDao {
      * @param pass パスワード
      * @return ユーザ情報（ResultSet）
      */
-    public ResultSet selectUser(String id, String name) {
+    public ResultSet selectUser(String id, String password) {
         try {
             // JDBCドライバのロード
             // 「com.mysql.jdbc.Driver」クラス名
-            Class.forName("com.mysql.jdbc.Driver");
-
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            
             // データベースと接続（本来はユーザやパスワードも別管理にしておくのが理想）
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sample?useUnicode=true&characterEncoding=utf8", SQL_ID, SQL_PASS);
+            con = DriverManager.getConnection(SQL_URL, SQL_ID, SQL_PASS);
 
             // SQL文を生成
-            ps = con.prepareStatement("select name, imgpath from user where id = ? and name = ?");
+            String sql = "SELECT name FROM person WHERE id = ? AND password = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
 
             // 生成したSQL文の「？」の部分にIDとパスワードをセット
             ps.setString(1, id);
-            ps.setString(2, name);
+            ps.setString(2, password);
 
             // SQLを実行
             rs = ps.executeQuery();
-            
-        } catch (ClassNotFoundException cne) {
-            cne.printStackTrace();
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
@@ -62,9 +60,6 @@ public class LoginDao {
             // データベースとの接続を解除する
             if (con != null) {
                 con.close();
-            }
-            if (ps != null) {
-                ps.close();
             }
             if (rs != null) {
                 rs.close();
