9bf5c41b6d56a5272d075d25fccd3245928b609f
palaw <palaw@users.noreply.github.com>
2016-12-04 13:53:04 +0900

    🎨 LoginDBの無駄なコメント削除

--- WEB-INF/src/login/LoginDB.java
+++ WEB-INF/src/login/LoginDB.java
@@ -4,29 +4,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * ログイン処理クラス. 
- * 
- * Loginクラスを親にしています
+ * ログイン処理クラス
  */
 public class LoginDB extends Login {
 
     public LoginUserBean getUserData(String id, String password) {
+
         LoginUserBean bean = null;
         LoginDao dao = null;
         ResultSet rs;
 
         try {
-            // DAOクラスをインスタンス化
+
             dao = new LoginDao();
-            // 画面で入力されたIDとパスワードを基にDB検索を実行
+            // DBを検索
             rs = dao.selectUser(id, password);
 
             while (rs.next()) {
-                /*検索結果が存在する場合はbeanに値をセット
-                 （結果が1件しか返らないことを想定）
-                 */
-                 bean = new LoginUserBean();
-                // ID（IDは引数のものをセット）
+
+                bean = new LoginUserBean();
+
+                // ID
                 bean.setId(id);
                 // 名前
                 bean.setName(rs.getString("name"));
@@ -34,7 +32,7 @@ public class LoginDB extends Login {
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         } finally {
-            // 処理終了時に各接続を解除
+            // 各接続を解除
             dao.close();
         }
         return bean;
