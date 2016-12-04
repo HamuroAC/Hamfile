0ed808c50c2f33fa97bcb937f0a9fb45b8b3ee54
palaw <palaw@users.noreply.github.com>
2016-12-04 13:50:10 +0900

    👍 LoginDaoのクローズするものを追加

--- WEB-INF/src/login/LoginDao.java
+++ WEB-INF/src/login/LoginDao.java
@@ -7,45 +7,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * DAOクラス.
- *
- * DaoFuctory
+ * ログインDAOクラス
  */
 public class LoginDao {
 
     private Connection con = null;
     private ResultSet rs = null;
+    private PreparedStatement ps = null;
 
     private final String SQL_URL = "jdbc:mysql://localhost:3306/teamA?autoReconnect=true&useSSL=false";
     private final String SQL_ID = "kunugi";
     private final String SQL_PASS = "hamuro";
 
     /**
-     * データベースから指定されたIDとパスワードを使ってユーザ情報を検索します.
+     * IDとPASSWORDを使ってユーザ情報を検索
      *
      * @param id	ログインID
-     * @param pass パスワード
+     * @param pass  パスワード
      * @return ユーザ情報（ResultSet）
      */
     public ResultSet selectUser(String id, String password) {
         try {
-            // JDBCドライバのロード
-            // 「com.mysql.jdbc.Driver」クラス名
+
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            
-            // データベースと接続（本来はユーザやパスワードも別管理にしておくのが理想）
+
+            // DBに接続
             con = DriverManager.getConnection(SQL_URL, SQL_ID, SQL_PASS);
 
-            // SQL文を生成
+            // SQLを生成
             String sql = "SELECT name FROM person WHERE id = ? AND password = ?";
             PreparedStatement ps = con.prepareStatement(sql);
 
-            // 生成したSQL文の「？」の部分にIDとパスワードをセット
+            // ?を設定
             ps.setString(1, id);
             ps.setString(2, password);
 
             // SQLを実行
             rs = ps.executeQuery();
+
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
@@ -53,19 +52,21 @@ public class LoginDao {
     }
 
     /**
-     * コネクションをクローズします.
+     * コネクションをクローズ
      */
     public void close() {
         try {
-            // データベースとの接続を解除する
+            // DB接続を解除
             if (con != null) {
                 con.close();
+            }
+            if (ps != null) {
+                ps.close();
             }
             if (rs != null) {
                 rs.close();
             }
-        } catch (SQLException sqle) {
-            // データベースとの接続解除に失敗した場合
+        }catch (SQLException sqle) {
             sqle.printStackTrace();
         }
     }
