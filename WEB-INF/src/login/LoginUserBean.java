6daeb6f0975281fe952f251460aa4fbf61f95c1a
palaw <palaw@users.noreply.github.com>
2016-12-04 13:54:15 +0900

    🎨 LoginUserBeanの無駄なコメント削除

--- WEB-INF/src/login/LoginUserBean.java
+++ WEB-INF/src/login/LoginUserBean.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package login;
 
 import java.io.Serializable;
 
 public class LoginUserBean implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     private String id;
     private String name;
 
@@ -17,24 +13,26 @@ public class LoginUserBean implements Serializable {
         this.id = "";
         this.name = "";
     }
-    
+
     public LoginUserBean(String id, String password) {
         this.id = id;
         this.name = name;
     }
-    
-    public void setId(String id){
+
+    public void setId(String id) {
         this.id = id;
     }
+
     public String getId(){
         return this.id;
     }
-    
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
        return this.name;
     }
-    
+
 }
