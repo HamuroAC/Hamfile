18bf0c83cba1bd0f72ef14c442280933b2607762
palaw <palaw@users.noreply.github.com>
2016-12-10 09:54:06 +0900

    👍 SignupServletを大幅に変更

--- WEB-INF/src/signup/SignupServlet.java
+++ WEB-INF/src/signup/SignupServlet.java
@@ -5,53 +5,59 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.MultipartConfig;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import javax.servlet.http.HttpSession;
 
+import login.LoginUserBean;
 /**
- * ログイン機能.
+ * 新規登録機能
  */
 @WebServlet(name = "SignupServlet", urlPatterns = { "/SignupServlet" })
-@MultipartConfig(maxFileSize = 1048576)
 public class SignupServlet extends HttpServlet {
+
     public SignupServlet() {
         super();
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        // ① クリックされたボタンの判定
-        // ①-1 ボタン名の文字化けを防ぐために文字コードを設定してから取得
+
+        // 文字化け防止
         request.setCharacterEncoding("UTF-8");
 
-        // ② 画面移動の準備
-        HttpSession session = request.getSession();	// セッション
-        RequestDispatcher rd;						// 画面の情報
+        HttpSession session = request.getSession();
+        RequestDispatcher rd;
 
-            // ③-1 クリックされたボタンが「ログイン」の場合はログイン処理を実施
-            // ③-1-1 ログイン画面で入力されたIDとパスワードを取得
+            // IDとPASSWORDを取得
             String id = request.getParameter("id");
             String password = request.getParameter("password");
-            // ③-1-2 ユーザ情報をモデルに格納するために指示
-            // ③-1-3 ログイン処理クラスをインスタンス化
-            SignupDB signupdb = new SignupDB();
 
-            // ③-1-4 ID処理クラスに②-1-1で取得したIDを渡してユーザ情報をモデルに格納
-            SignupUserBean bean = signupdb.getUserData(id, password);
-             // ③-2 モデルの情報を判定
-            if (bean == null) {
+            SignupDB db = new SignupDB();
 
-                // ③-2-2 つぎに表示させる画面（ビュー）を指定
-                rd = request.getRequestDispatcher("./edit.jsp");
+            try {
 
-            } else {
+                LoginUserBean bean = db.insertUserData(id, password);
+                // ログイン判定
 
-                rd = request.getRequestDispatcher("./index.html");
+                if (bean != null) {
+                    // ユーザー情報
+                    session.setAttribute("user_db", bean);
+                    session.setAttribute("login_db", "login");
+                    // セッション有効時間
+                    session.setMaxInactiveInterval(180);
+                    rd = request.getRequestDispatcher("./edit.jsp");
+
+                } else {
+                    rd = request.getRequestDispatcher("./index.html");
+                }
+                rd.forward(request, response);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            rd.forward(request, response);
-        }
     }
+}
