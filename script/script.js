3a23d8b5ad111ac32dab90c8d88baa272a10794d
euphmat <1202pirom@gmail.com>
2017-01-14 20:55:11 +0900

    ✨トップページの説明画像を自動スクロール機能実装
    マウスでドラッグもできるよ

--- script/script.js
+++ script/script.js
@@ -1,32 +1,42 @@
 //絵のスクロール
 document.addEventListener("DOMContentLoaded", function () {
   [].forEach.call(document.querySelectorAll('.dropimage'), function (img) {
-    img.onchange = function (e) {
-      var inputfile = this,
-        reader = new FileReader();
-      reader.onloadend = function () {
-        inputfile.style['background-image'] = 'url(' + reader.result + ')';
-      }
-      reader.readAsDataURL(e.target.files[0]);
-    }
-  });
+		img.onchange = function (e) {
+			var inputfile = this,
+				reader = new FileReader();
+			reader.onloadend = function () {
+				inputfile.style['background-image'] = 'url(' + reader.result + ')';
+			}
+			reader.readAsDataURL(e.target.files[0]);
+		}
+	});
 });
 //モーダルをキーボード操作
 document.onkeydown = function (e) {
-  if (e.keyCode == 27) {
-    var mods = document.querySelectorAll('.modal > [type=checkbox]');
+	if (e.keyCode == 27) {
+		var mods = document.querySelectorAll('.modal > [type=checkbox]');
     [].forEach.call(mods, function (mod) {
-      mod.checked = false;
-    });
-  }
+			mod.checked = false;
+		});
+	}
 }
 
 //利用規約判定
 function check() {
-  if (document.getElementById("p_checkbox").checked) {
-    return true;  //送信実行
-  } else {
-    alert("利用規約に同意してください");
-    return false; // 送信中止
-  }
+	if (document.getElementById("p_checkbox").checked) {
+		return true; //送信実行
+	} else {
+		alert("利用規約に同意してください");
+		return false; // 送信中止
+	}
 }
+
+//slick
+$(document).ready(function () {
+	$('.desimg_center').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+});
