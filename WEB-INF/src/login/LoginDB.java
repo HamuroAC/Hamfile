ffd0e19c520bcba3de05612aa3c62ea77e4388dc
KUNUGIDAMA <kd1192088@st.kobedenshi.ac.jp>
2017-01-16 14:19:39 +0900

    👍imagepath.profileの追加のための変更処理
    セットされる処理内容を変更、データセットでの返り値を修正

--- WEB-INF/src/login/LoginDB.java
+++ WEB-INF/src/login/LoginDB.java
@@ -18,6 +18,8 @@ public class LoginDB extends Login {
 
             dao = new LoginDao();
             // DBを検索
+            //rs = dao.selectUser(id, password);
+            //上記の処理を追加項目修正
             rs = dao.selectUser(id, password);
 
             while (rs.next()) {
@@ -28,6 +30,11 @@ public class LoginDB extends Login {
                 bean.setId(id);
                 // 名前
                 bean.setName(rs.getString("name"));
+                //画像PASS
+                bean.setImagepath(rs.getString("imagepath"));
+                //profileの本文を追加
+                bean.setProfile(rs.getString("profile"));
+                
             }
         } catch (SQLException sqle) {
             sqle.printStackTrace();
