0b288493545214ab9ab09b8bb16f0b1ee183db2f
euphmat <1202pirom@gmail.com>
2017-01-20 13:59:30 +0900

    👍例外処理から画像判定を削除

--- script/script.js
+++ script/script.js
@@ -35,15 +35,11 @@ $(function(){
 	$('#editsubmit').on('click',function(){
 		var self = $('#self').val();
 		var editname = $('#editname').val();
-		var editimg = $('#editimg').val();
 		if(self.length == 0){
 			alert("自己紹介を入力してください。");
 			return false;
 		}else if(editname.length == 0){
 			alert("名前を入力してください。");
-			return false;
-		}else if(editimg.length == 0){
-			alert("画像を添付してください。");
 			return false;
 		}else{
 			return true;
